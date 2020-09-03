5494bb40b6b81110967cd212713527c927a6e43b
ziyainan <inanzya@gmail.com>
2020-09-03 05:54:02 -0400

    Elionun dersi

---
+++ src/Collection/InitializeArray.java
@@ -0,0 +1,43 @@
+package Collection;
+
+import com.sun.jmx.remote.internal.ArrayQueue;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class InitializeArray {
+
+    public static void main(String[] args) {
+
+        // Array --> you can store obj or primitive
+        // ArrayList --> keep only obj  obj "int" should be "Integer"
+
+        ArrayList <Integer> list1= new ArrayList<Integer>();
+
+        List<Integer>a= new ArrayList<>();
+        a.add(1);
+        a.add(2);
+
+        
+
+
+
+        System.out.println(a);
+
+//       Collections.addAll(list1,1,2);
+//       System.out.println(list1);
+//       for(Integer zy: list1){
+//           System.out.println(zy);
+
+       }
+
+
+
+
+
+
+    }
+
+
+
